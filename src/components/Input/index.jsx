b5299ae6df02d2130bcd1e3cf7093972c1923dda
rafacarrenho <rafael.carrenho@hotmail.com>
2021-05-13 20:45:36 -0300

    Refactor - Modified class to function components

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,15 +1,8 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
+import { useEffect, useRef, useState, useCallback } from "react";
+import { useField } from "@unform/core";
+import { Container } from "./styles";
 
-import { useField } from '@unform/core';
-
-import { Container } from './styles';
-
-const Input = ({ name, icon: Icon, ...rest }) => {
+export const Input = ({ name, icon: Icon, ...rest }) => {
   const inputRef = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
@@ -31,7 +24,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
@@ -49,5 +42,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
